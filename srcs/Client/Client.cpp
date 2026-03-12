94464d9eabe897acf2faee2953500f904ee27513
lsalmon <lsalmon@student.42.nice.fr>
2026-03-12 17:10:44 +0100

    class Client OK pour test

---
+++ srcs/Client/Client.cpp
@@ -0,0 +1,70 @@
+#include "Client.hpp"
+#include <arpa/inet.h>
+
+Client::Client() : _ops(false), _fd(-1) {
+}
+
+Client::Client(sockaddr_in addrClient, pollfd Socketclient)
+	: _ops(false), _fd(Socketclient.fd) {
+	this->_adress = inet_ntoa(addrClient.sin_addr);
+	std::cout << "New client connected: " << this->_adress << std::endl;
+	std::cout << "Client fd: " << this->_fd << std::endl;
+	std::cout << "Client port: " << ntohs(addrClient.sin_port) << std::endl;
+}
+
+Client &Client::operator=(const Client &other) {
+	if (this != &other) {
+		this->_ops = other._ops;
+		this->_fd = other._fd;
+		this->_password = other._password;
+		this->_adress = other._adress;
+		this->_nick = other._nick;
+		this->_second = other._second;
+		this->_third = other._third;
+		this->_username = other._username;
+	}
+	return *this;
+}
+
+Client::~Client() {
+}
+
+std::string Client::getPassword() {
+	return this->_password;
+}
+
+std::string Client::getAdress() {
+	return this->_adress;
+}
+
+std::string Client::getNick() {
+	return this->_nick;
+}
+
+std::string Client::getSecond() {
+	return this->_second;
+}
+
+std::string Client::getThird() {
+	return this->_third;
+}
+
+std::string Client::getUser() {
+	return this->_username;
+}
+
+int Client::cmdNick(const std::string& newNick, const Client& user) {
+	(void)user;
+	this->_nick = newNick.substr(0, 30);
+	return 0;
+}
+
+int Client::cmdQuit(const Client& user) {
+	(void)user;
+	return 0;
+}
+
+int Client::cmdReconnect(const Client& user) {
+	(void)user;
+	return 0;
+}
